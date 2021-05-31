d660aecc1949a09f733e53e14ef2e128b229cd9b
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-05-31 19:53:44 +0200

    Add shared lib to decompress xRIT data in-memory, accessible via extern-c / python interface.

---
+++ xRITDecompress/libxRITDecompress.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <istream>
+#include <streambuf>
+#include <cstring>
+#include <memory>
+
+#include "CxRITFile.h"				// DISE
+#include "CxRITFileDecompressed.h"	// DISE
+
+const char* VERSION_NUMBER="2.7.2";
+
+#define EXTERN_DLL_EXPORT extern "C"
+#ifdef _WIN32
+#define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
+#endif
+
+class xRITWrapper {
+public:
+    xRITWrapper(char* in_buffer, int in_bytes);
+    unsigned long long getOutputLength() const;
+    void write(char* return_buffer);
+private:
+    std::ostringstream output_buffer;
+    unsigned long long output_length = 0;
+};
+
+xRITWrapper::xRITWrapper(char *in_buffer, int in_bytes) {
+    std::istringstream in_stream(std::string(in_buffer, in_bytes));
+    // Open input file.
+    DISE::CxRITFile compressedFile;
+    try {
+        compressedFile = DISE::CxRITFile(in_stream);
+    } catch (...) {
+        std::cerr << "Opening/reading input file failed.\n";
+    }
+    // Decompress input file.
+    DISE::CxRITFileDecompressed decompressedFile(compressedFile);
+    decompressedFile.Write(output_buffer);
+    output_buffer.seekp(0, std::ios::end);
+    output_length = output_buffer.tellp();
+}
+
+unsigned long long xRITWrapper::getOutputLength() const {
+    return output_length;
+}
+
+void xRITWrapper::write(char* return_buffer) {
+    std::memcpy(return_buffer, output_buffer.str().c_str(), output_length);
+}
+
+
+// Define C functions for the C++ class - as ctypes can only talk to C...
+EXTERN_DLL_EXPORT {
+    xRITWrapper* xRITWrapper_Constructor(char* in_buffer, int in_bytes) {
+        return new xRITWrapper(in_buffer, in_bytes);
+    }
+    unsigned long long xRITWrapper_getOutputLength(xRITWrapper* foo) {
+        return foo->getOutputLength();
+    }
+    void xRITWrapper_write(xRITWrapper* foo, char* out_buffer) {
+        return foo->write(out_buffer);
+    }
+}
