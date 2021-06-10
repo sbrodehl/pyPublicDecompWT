63b34583843b5286af556037bdef6748094cb61a
tonucci <alessandro.tonucci@arakne.it>
2021-06-10 12:41:54 +0200

    Commit relate to issue #1
    added first version of configuration files for conda packages

---
+++ conda/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#$CXX -fPIC -g -Wall *.cpp -shared -o gdal_MSG.so -I $PREFIX/include/ -std=c++11 -L. -L $PREFIX/lib/ -l gdal -I PublicDecompWT/DISE/ -I PublicDecompWT/COMP/Inc/ -I PublicDecompWT/COMP/WT/Inc/
+
+make -C xRITDecompress 
+
+
+
