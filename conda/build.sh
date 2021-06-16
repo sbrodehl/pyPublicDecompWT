22e97d09bfb18e1218003d7c3a8bdee2e25e1466
tonucci <alessandro.tonucci@arakne.it>
2021-06-16 10:11:02 +0200

    Issue #1: added install target in order to include the xRITDecompress executable to the package

--- conda/build.sh
+++ conda/build.sh
@@ -3,7 +3,6 @@
 
 #$CXX -fPIC -g -Wall *.cpp -shared -o gdal_MSG.so -I $PREFIX/include/ -std=c++11 -L. -L $PREFIX/lib/ -l gdal -I PublicDecompWT/DISE/ -I PublicDecompWT/COMP/Inc/ -I PublicDecompWT/COMP/WT/Inc/
 
-make -C xRITDecompress 
-
+make -C xRITDecompress install DEST_DIR=$PREFIX
 
 
