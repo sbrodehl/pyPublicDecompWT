7d7652a86cb0a2b2dea8cef4b03109bf24026c2a
S. Brodehl <foss@s14l.de>
2024-04-20 16:49:56 +0200

    Update Python versions.

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "Intended Audience :: Developers",
     "Intended Audience :: Science/Research",
     "Topic :: Scientific/Engineering :: Atmospheric Science",
